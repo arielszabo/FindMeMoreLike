2c3485e066d8796c7997307eb9af075f75a1d1af
arielszabo <arielszabo@gmail.com>
2019-08-12 17:01:08 +0300

    refactor - cahnge the extract_data method and create a new method to implament

--- more_like/extraction/extractors.py
+++ more_like/extraction/extractors.py
@@ -5,6 +5,7 @@ import glob
 import requests
 import time
 import logging
+import BeautifulSoup
 
 class DataExtractor(object):
     def __init__(self, project_config, saving_path):
@@ -16,8 +17,23 @@ class DataExtractor(object):
         all_saved_files = glob.glob(os.path.join(self.saving_path, '*.json'))
         return list(map(lambda name: re.search(r'tt\d+', name).group(0), all_saved_files))
 
-    def extract_data(self, ids_to_query):
+    def _extract_a_single_id(self, movie_id): # This method needs to be implemented
         pass  # This method needs to be implemented todo: add an assertion if not with the class name
+
+    def extract_data(self, ids_to_query):
+        """
+        Query and save movie data. Alert and raise errors if we reach the request limit or if the 'Response' is false.
+        :param [list] ids_to_query: A list of movie ids to query the IMDB API
+        :return: None, save the data json files
+        """
+        for i, movie_id in enumerate(ids_to_query):
+            if movie_id not in self.existing_ids: # if it's already existing then don't query it # todo: add a better cache invalidation
+                response_data = self._extract_a_single_id(movie_id) # This method needs to be implemented
+                if response_data:  # if it's not None
+                    self.save(data=response_data, movie_id=movie_id)
+
+            percent_queried = 100 * (i + 1) / len(ids_to_query)
+            logging.info('Finished: {}%'.format(round(percent_queried, 2)))
 
     def save(self, data, movie_id):
         with open(os.path.join(self.saving_path, '{}.json'.format(movie_id)), 'w') as j_file:
@@ -25,21 +41,24 @@ class DataExtractor(object):
 
 
 class IMDBApiExtractor(DataExtractor):
+    def __init__(self, *args, **kwargs): # todo: is this the best way?
+        super().__init__(*args, **kwargs)
+        self.user_api_key = self._get_user_api_key()
+
     def _get_user_api_key(self):
         with open(self.project_config['user_api_key_path']) as f:
             user_api_key = f.read()
         return user_api_key
 
-    @staticmethod
-    def _extract_a_single_id(movie_id, user_api_key):
+    def _extract_a_single_id(self, movie_id):
         response = requests.get('http://www.omdbapi.com/?i={}&apikey={}&?plot=full'.format(movie_id,
-                                                                                           user_api_key))
+                                                                                           self.user_api_key))
 
         if response.json() == {"Error": "Request limit reached!", "Response": "False"}:
             logging.info("Request limit reached! Lets wait 24 hours")
             time.sleep(86500)
             response = requests.get('http://www.omdbapi.com/?i={}&apikey={}&?plot=full'.format(movie_id,
-                                                                                               user_api_key))
+                                                                                               self.user_api_key))
             # raise TypeError("Request limit reached!")
 
         if response.json()['Response'] == 'False':
@@ -48,24 +67,4 @@ class IMDBApiExtractor(DataExtractor):
             # raise ValueError("Response == False ? at {}".format(movie_id))
 
         return response
-
-    def extract_data(self, ids_to_query):
-        """
-        Query and save movie data. Alert and raise errors if we reach the request limit or if the 'Response' is false.
-        :param [list] ids_to_query: A list of movie ids to query the IMDB API
-        :return: None, save the data json files
-        """
-        user_api_key = self._get_user_api_key()  # todo: is this a good way of doing this?
-
-        logging.info('Extract {} movies data: {}'.format(len(ids_to_query), ids_to_query))
-
-        for i, movie_id in enumerate(ids_to_query):
-            if movie_id not in self.existing_ids: # if it's already existing then don't query it # todo: add a better cache invalidation
-                response_data = self._extract_a_single_id(movie_id, user_api_key)
-                if response_data:  # if it's not None
-                    self.save(data=response_data, movie_id=movie_id)
-
-            percent_queried = 100 * (i + 1) / len(ids_to_query)
-            logging.info('Finished: {}%'.format(round(percent_queried, 2)))
-
 
