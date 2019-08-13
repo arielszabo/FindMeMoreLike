5027d89120878e61ab9180f37ff8eef858009a20
arielszabo <arielszabo@gmail.com>
2019-08-13 14:17:09 +0300

    add tokenizer to text

---
+++ more_like/vectorization/text_vectors.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import string
+
+
+def token_text(full_text, remove_stop_words=True, remove_punctuations=True, remove_if_not_alpha=True, stem_word=False):
+    porter = PorterStemmer()
+
+    stop_words = set(stopwords.words('english'))
+    punctuations = list(string.punctuation)
+
+    tokenize_words = []
+    for sentence in nltk.sent_tokenize(full_text.lower()):
+        for word in nltk.word_tokenize(sentence):
+            if word in stop_words and remove_stop_words == True:
+                continue
+            if word in punctuations and remove_punctuations == True:
+                continue
+            if not word.isalpha() and remove_if_not_alpha == True:  # if not all characters in the string are alphabetic
+                continue
+            if stem_word:
+                word = porter.stem(word)
+
+            tokenize_words.append(word)
+    return tokenize_words
