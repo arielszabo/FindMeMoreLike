999e8a0dc12080dadd1147cad81ead59eec1c7ae
arielszabo <arielszabo@gmail.com>
2019-11-08 15:16:20 +0200

    wip

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,7 +1,8 @@
 import pytest
 import os
 
-@pytest.mark.skipif(os.environ.get("TRAVIS") == True, reason="needs lot of data")
+print(os.environ)
+@pytest.mark.skipif(os.environ.get("TRAVIS") == "true", reason="needs lot of data")
 def test_all():
     assert 1 == 2
 
