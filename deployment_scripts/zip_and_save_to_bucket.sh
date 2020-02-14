a5fd0b0abf36cd60426264e75b2c20a98e3f591a
arielszabo <arielszabo@gmail.com>
2020-02-14 12:44:10 +0200

    save zipping logs

--- deployment_scripts/zip_and_save_to_bucket.sh
+++ deployment_scripts/zip_and_save_to_bucket.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
+zipping_log_file_path=find_me_more_like_logs/zipping_log_file.txt
+rm ${zipping_log_file_path}
 
 zip_folder_containing_folders_and_save () {
+    $1 >> zipping_log_file_path
     mkdir ${1}_zip
     cd $1
     for i in */; do tar -zcvf "../${1}_zip/${i%/}.tar.gz" "$i"; done
     cd ..
 
     # save results in bucket
-    gsutil -m cp -r "${1}_zip" gs://ariel-szabo/find-me-more-like/raw_data_zip_files/
+    gsutil -m cp -r "${1}_zip" gs://ariel-szabo/find-me-more-like/raw_data_zip_files/ >>zipping_log_file_path 2>>zipping_log_file_path
 }
-
-
-# save logs in bucket
-gsutil -m cp -r find_me_more_like_logs gs://ariel-szabo/find-me-more-like/
-gsutil -m cp webapp/static/title_and_id_mapping.json gs://ariel-szabo/find-me-more-like/
-
 
 
 zip_folder_containing_folders_and_save similar_list_data
@@ -23,6 +20,9 @@ zip_folder_containing_folders_and_save similar_list_data
 #zip_folder_containing_folders_and_save raw_wiki_data
 zip_folder_containing_folders_and_save vectors_cache
 
+
+gsutil -m cp -r find_me_more_like_logs gs://ariel-szabo/find-me-more-like/
+gsutil -m cp webapp/static/title_and_id_mapping.json gs://ariel-szabo/find-me-more-like/
 
 sleep 60
 # stop instance
